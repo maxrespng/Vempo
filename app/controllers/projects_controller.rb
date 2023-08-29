a7e1804ffe006dcaf8ee336207d242b1a21ae58c
Pinar Boztepe <pinarikamu@hotmail.com>
2023-08-29 15:22:43 +0100

    Create action created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,11 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def create
+    @project = Project.new
+    @project.user = current_user
+    @project.save!
+    redirect_to project_path(@project)
+  end
+
 end
