b3e5ecf7158fd249924493515118d96845d79866
Maximilian Lana <maxlana@gmx.co.uk>
2023-08-31 17:23:22 +0100

    made shape controller and create action

--- app/javascript/controllers/new_shape_controller.js
+++ app/javascript/controllers/new_shape_controller.js
@@ -1,35 +1,15 @@
-
-// import { Controller } from "@hotwired/stimulus"
-
-// // Connects to data-controller="new-shape"
-// export default class extends Controller {
-//   static targets = ["p5Canvas", "checkbox", "checkboxes"]
-//   connect() {
-//     console.log("all connected!")
-//   }
-
-
-//  draw() {
-//   this.checkboxesTarget.foreach
-//   const checkbox = this.checkboxTarget
-//   const shape = checkbox.dataset.shape
-//   if (shape === "triangle") {
-//     console.log("im a triangle")
-//   }
-
-
-// }
-// }
-
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["p5Canvas", "checkboxes", "checkbox", "input1", "input2", "input3", "input4", "colorPicker"]
   static values = {
-    input: String
+    input: String,
+    url: String
   }
 
   connect() {
+    console.log({ url: this.urlValue })
+
     this.shape
     this.userCanDraw = false;
     // Agregar un evento click a cada checkbox
@@ -44,7 +24,7 @@ export default class extends Controller {
     let target = event.target;
     console.log(this.checkboxTargets.indexOf(target))
     while (this.checkboxTargets.indexOf(target) == -1) {
-      target =  target.parentElement;
+      target = target.parentElement;
       console.log(this.checkboxTargets.indexOf(target))
     }
     console.log("Found element")
@@ -97,6 +77,7 @@ export default class extends Controller {
   }
 
   draw(mouse_x, mouse_y, newMouse_x, newMouse_y) {
+    console.log('firing draw')
     let name = ''
     mouse_x = parseInt(mouse_x, 10);
     mouse_y = parseInt(mouse_y, 10);
@@ -106,58 +87,67 @@ export default class extends Controller {
         console.log("this is a triangle")
         triangle(mouse_x, mouse_y - 50, newMouse_x + 100, newMouse_y, mouse_x + 200, mouse_y);
         // trigger save/update method
+        fill(selectedColor);
         name = 'triangle'
       }
       else if (this.shape === "circle") {
         console.log("Circle");
         circle(mouse_x, mouse_y - 50, 55)
         // trigger save/update method
+        fill(selectedColor);
         name = 'circle'
       }
       else if (this.shape === "square") {
         square(mouse_x, mouse_y - 50, newMouse_x);
         // trigger save/update method
+        fill(selectedColor);
         name = 'square'
 
       }
       else if (this.shape === "oval") {
-        ellipse(mouse_x, mouse_y - 50, newMouse_x);
+        ellipse(mouse_x, mouse_y - 50, newMouse_x - mouse_x);
         // trigger save/update method
+        fill(selectedColor);
         name = 'oval'
       }
       else if (this.shape === "rectangle") {
         rect(mouse_x, mouse_y - 50, newMouse_x - mouse_x, newMouse_y - mouse_y);
         // trigger save/update method
+        fill(selectedColor);
         name = 'rectangle'
         let shapeData = { name: name, start_x: mouse_x, start_y: mouse_y, width: newMouse_x - mouse_x, height: newMouse_y - mouse_y };
-        console.log(shapeData);
-        fill(selectedColor);
+        // console.log(shapeData);
+        this.saveShape(shapeData)
       }
       // const data = JSON.stringify
 
     }
   }
+
+  saveShape(shapeData) {
+    const url = this.urlValue
+
+    console.log(url);
+    console.log({ shapeData });
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        shapeData
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+  }
 }
 
 // write a save/update function below
 
-// saveShape() {
-//   fetch('url', {
-//     fetch("https://jsonplaceholder.typicode.com/todos", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         userId: 1,
-//         title: "Fix my bugs",
-//         completed: false
-//       }),
-//       headers: {
-//         "Content-type": "application/json; charset=UTF-8"
-//       }
-//     });
-// }
 
 
-// Today's task:
+
+// Today's task (31/08/2023):
 // 1. create form in sidebar to contain all the data for our shapes (start_x, start_y, etc.)
 // 2. implement a mouseDown event listener that records the start_x and start_y of the shape
 // 3. implement a mouseUp event listener that records the width and height values for our shape.
