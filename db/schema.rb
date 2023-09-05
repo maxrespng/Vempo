3aa57563cd059da01cd9d7512b7d26f1592220c5
Helder Victoretti <heldervictoretti@yahoo.com.br>
2023-09-05 12:49:47 +0100

    added screenshot funcionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_081315) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_095026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_081315) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "svg"
+    t.text "screenshot"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
